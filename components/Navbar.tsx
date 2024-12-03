4d7c8ae6c611e3379ab30eecf6a8cd2823250caf
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-12-03 11:06:04 +0900

    init

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="h-[50px] w-full bg-pink-500 justify-items-center">
+        <div className="container flex flex-row justify-between h-full items-center ">
+            <p className="text-2xl text-center">취미 리스트</p>
+            </>
+        </div>
+
+    </nav>
+)}
+
+export default Navbar
