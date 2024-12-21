88571baf332350a9858863a001c6919b1365a66e
njoyshadow <shs3740413@gmail.com>
2024-12-21 17:39:06 +0900

    update url

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     const [hobbies, setHobbies] = useState<HobbyItemProps[]>([]);
     useEffect(() => {
         setHobbies([])
-        axios.get(`${import.meta.env.VITE_API_URL}/api/hobby/`).then((res) => {
+        axios.get(`${import.meta.env.VITE_API_URL}/api/hobby`).then((res) => {
             if (res.status !== 200)
                 return
             res.data.map((x: HobbyItemProps) => {
